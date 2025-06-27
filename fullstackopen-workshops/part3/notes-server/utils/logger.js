61843adff25fe247b2bceb20d3e71e243e99d430
Akash Tolange <akashhtolangee77@gmail.com>
2025-06-27 12:05:56 +0545

    Add info arrow func

---
+++ fullstackopen-workshops/part3/notes-server/utils/logger.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+//conosle.log in our terminal so not a good idea vayeko le 
+
+const info =(...params) =>{
+    console.log(...params);
+}
+
+module.exports ={
+    info,
+}
