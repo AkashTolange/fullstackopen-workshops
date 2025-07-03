28465bdae53ba9c16b0e0699a79221cd007cdcd1
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-03 23:07:40 +0545

    Add conditionally log if NODE_ENV is not test in logger

--- fullstackopen-workshops/part3/notes-server/utils/logger.js
+++ fullstackopen-workshops/part3/notes-server/utils/logger.js
@@ -2,7 +2,12 @@
 //conosle.log in our terminal so not a good idea vayeko le 
 
 const info =(...params) =>{
-    console.log(...params);
+    if(process.env.NODE_ENV !== 'test') {
+        // console.log("NODE_ENV is :", process.env.NODE_ENV);
+        // console.log("params are :", params);
+        // console.log("params are :", ...params);
+        console.log(...params);
+    }
 }
 
 module.exports ={
