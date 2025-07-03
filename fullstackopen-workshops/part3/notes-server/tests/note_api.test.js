baf9e3cddbfaa4195a0e4016397e582444193b11
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-03 23:05:35 +0545

    Add initialNotes & connect mongodb & testing one at a time

--- fullstackopen-workshops/part3/notes-server/tests/note_api.test.js
+++ fullstackopen-workshops/part3/notes-server/tests/note_api.test.js
@@ -4,13 +4,35 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
+const Note = require('../models/note')
 //wrap garyo app lae    
-const api = supertest(app)
+
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+const api = supertest(app);
 
 test('notes are returned as json', async () => {
   await api
-    .get('/api/notes')
-    .expect(200)
+  .get('/api/notes')
+  .expect(200)
     // .expect('Content-Type', 'application/json; charset=utf-8')
     // .expect('Content-Type', /application\/json/)
     .expect('Content-Type', /application\/json/);
@@ -19,7 +41,7 @@ test('notes are returned as json', async () => {
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
   
-  expect(response.body).toHaveLength(6);
+  expect(response.body).toHaveLength(initialNotes.length);
   // expect(response.body.length).toBe(3)
 //   assert.strictEqual(response.body.length, 2)
 })
@@ -27,7 +49,7 @@ test('all notes are returned', async () => {
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
   
-  expect(response.body[0].content).toBe('HTML is easy');
+  expect(response.body[0].content).toBe('initialNotes[0].content');
 //   const contents = response.body.map(e => e.content)
 //   assert.strictEqual(contents.includes('HTML is easy'), true)
 })
