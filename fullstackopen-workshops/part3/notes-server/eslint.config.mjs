06a146709ab8da88d476c49a9288d3381197a1e6
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-03 17:20:07 +0545

    Remaining config and jest true

--- fullstackopen-workshops/part3/notes-server/eslint.config.mjs
+++ fullstackopen-workshops/part3/notes-server/eslint.config.mjs
@@ -1,4 +1,4 @@
-import globals from "globals";
+import globals, { jest } from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -6,6 +6,17 @@ export default defineConfig([
     files: ["**/*.js"],
     languageOptions: {
       sourceType: "commonjs",
+      globals: {
+        test: true,
+        expect: true,
+        describe: true,
+        it: true,
+        beforeEach: true,
+        afterEach: true,
+        beforeAll: true,
+        afterAll: true,
+        jest: true,
+      },
     },
   },
   {
