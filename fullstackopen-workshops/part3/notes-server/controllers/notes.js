b5216e7e162ac707f2b26bc38578758e7738a397
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-06 13:44:00 +0545

    Add async & await & try catch block for error handling

--- fullstackopen-workshops/part3/notes-server/controllers/notes.js
+++ fullstackopen-workshops/part3/notes-server/controllers/notes.js
@@ -1,33 +1,54 @@
-const app = require("express").Router()
+const app = require("express").Router();
 const Note = require("../models/note");
 
-app.get("/", (request, response) => {
-  Note.find({}).then((result) => {
-    response.json(result);
-  });
-})
-
-//creating the '/api/notes/:id' route for a 'get' method request
-app.get("/:id", (request, response, next) => {
-    Note.findById(request.params.id).then(result =>{ 
-    // const myId = request.params.id;
-    // const myNote = notes.find((note) => note.id === myId); //enter note object is returned
-    //what if it does not find then what 
-    if (result ){
-        response.json(result);
-    } else {
-        response.status(404).send(`There are no notes at${request.params.id}`); //not found status
-    }
-   }).catch((e) =>{
-    next(e);
-    // console.log(err);
-    // response.status(500).send(`${request.params.id} is not in the required format`)
-   })
-    
- 
+app.get("/", async (request, response) => {
+  let result = await Note.find({});
+  response.json(result);
 });
 
-//put 
+// app.get("/", (request, response) => {
+//   Note.find({}).then((result) => {
+//     response.json(result);
+//   });
+// })
+
+//creating the '/api/notes/:id' route for a 'get' method request
+// app.get("/:id", (request, response, next) => {
+//   Note.findById(request.params.id)
+//     .then((result) => {
+//       // const myId = request.params.id;
+//       // const myNote = notes.find((note) => note.id === myId); //enter note object is returned
+//       //what if it does not find then what
+//       if (result) {
+//         response.json(result);
+//       } else {
+//         response.status(404).send(`There are no notes at${request.params.id}`); //not found status
+//       }
+//     })
+//     .catch((e) => {
+//       next(e);
+//       // console.log(err);
+//       // response.status(500).send(`${request.params.id} is not in the required format`)
+//     });
+// });
+
+
+//using async and await 
+app.get("/:id", async (request, response, next) => {
+   try {
+    const note = await Note.findById(request.params.id);
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+   }
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+
+//put
 // app.put('/api/notes/:id' , (request, response) => {
 //  const myId = Number(request.params.id);
 //  const updatedNote = request.body;
@@ -48,24 +69,24 @@ app.get("/:id", (request, response, next) => {
 // })
 
 //put - IMPORTANT: This was commented out and has been fixed
-app.put('/:id' , (request, response, next) => {
+app.put("/:id", (request, response, next) => {
   // const myId = request.params.id; // id from URL is a string
   const body = request.body; // updated note data from request body
 
-  const note ={
+  const note = {
     content: body.content,
-    important: body.important
+    important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true})
-  .then((updatedNote) => { 
-    response.json(updatedNote);
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
   })
-  .catch((error) => next(error));
-  
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
-
-
 
 //creating the /api/notes/:id route for a 'delete' method request
 // app.delete("/api/notes/:id", (request, response) => {
@@ -76,16 +97,28 @@ app.put('/:id' , (request, response, next) => {
 //     // response.status(204).end();
 // })
 
-app.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
-})
+// app.delete("/:id", (request, response, next) => {
+//   Note.findByIdAndRemove(request.params.id)
+//     .then((result) => {
+//       response.status(204).end();
+//     })
+//     .catch((error) => next(error));
+// });
+
+//using await and async
+//creating the /api/notes/:id route for a 'delete' method request
+
+app.delete("/:id", async (request, response, next) => {
+   try {
+    await Note.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
+});
 
 //creating the 'api/note' route for a 'post' method request
-// app.post("/api/notes", (request, response) => { 
+// app.post("/api/notes", (request, response) => {
 //     const myNewPost = request.body;
 //     // console.log(myNewPost);
 //     myNewPost.id =notes.length + 1;
@@ -103,27 +136,34 @@ app.delete('/:id', (request, response, next) => {
 // // })
 // })
 
-app.post('/', (request, response, next) =>{
+app.post("/", async (request, response, next) => {
   const body = request.body;
-  
-  //new post banauda khere handle garxa error handle garxa schema le tara update garda herdainw 
-  //this error handling no need as we have defined that error handling built in xa , mongoose ko schema error handling mw herxa  
+
+  //new post banauda khere handle garxa error handle garxa schema le tara update garda herdainw
+  //this error handling no need as we have defined that error handling built in xa , mongoose ko schema error handling mw herxa
   // if( body.content === undefined){
   //   return response.status(400).json({error: "content missing"});
   // }
 
-const note = new Note({
-  content: body.content,
-  important: body.important || false,
-});
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
 
-note.save().then((saveNote) => {
-  response.json(saveNote);
-}).catch( e => {
-  // console.log(e);
-  next(e);
-});
+  // note.save().then((saveNote) => {
+  //   response.status(201).json(saveNote);
+  // }).catch( e => {
+  //   // console.log(e);
+  //   next(e);
+  // });
 
-})
+  //using async/await
+  try { 
+    const saveNote = await note.save();
+    response.status(201).json(saveNote);
+  } catch (error) {
+      next(error);
+  }
+});
 
 module.exports = app;
