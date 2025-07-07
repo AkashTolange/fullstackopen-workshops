95a2fdd350aa6bb924bf008d2872657a2e041851
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-07 13:47:17 +0545

    Add JsonWebTokenError for error

--- fullstackopen-workshops/part3/notes-server/utils/middleware.js
+++ fullstackopen-workshops/part3/notes-server/utils/middleware.js
@@ -28,6 +28,9 @@ const errorHandler = (error, request, response, next) => {
   if( error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
