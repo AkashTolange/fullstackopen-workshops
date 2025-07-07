8c9155ad14b21118293769bca3f5f4515f79f201
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-07 14:18:27 +0545

    Add TokenExpiredError for Token

--- fullstackopen-workshops/part3/notes-server/utils/middleware.js
+++ fullstackopen-workshops/part3/notes-server/utils/middleware.js
@@ -31,6 +31,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: error.message })
   }
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' })
+  }
 
   next(error)
 }
