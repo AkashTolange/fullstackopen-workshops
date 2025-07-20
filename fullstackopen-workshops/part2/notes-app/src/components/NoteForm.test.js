0581762270ff7efb35fd97de5375bf5b5b2c8601
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-20 16:42:55 +0545

    Add testing the forms Note

---
+++ fullstackopen-workshops/part2/notes-app/src/components/NoteForm.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import NoteForm from './NoteForm'
+import userEvent from '@testing-library/user-event'
+import "@testing-library/jest-dom";
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+  const createNote = jest.fn() //dummy function to mock the onSubmit prop
+  const user = userEvent.setup()
+
+ let {container} = render(<NoteForm onSubmit={createNote} />)
+
+//   const input = screen.getByRole('textbox')
+//   const input = screen.getByPlaceholderText('enter something here');
+  const input = container.querySelector('.someThing')
+  const sendButton = screen.getByText('save')
+
+  await user.type(input, 'testing a form...')
+  await user.click(sendButton)
+
+  expect(createNote.mock.calls).toHaveLength(1)
+  expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
+})
