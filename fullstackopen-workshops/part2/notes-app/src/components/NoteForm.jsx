adad6a977fc13e72516f7756c1fce943179cfdcf
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-20 16:44:25 +0545

    Add className placeholder value & onSubmit function

--- fullstackopen-workshops/part2/notes-app/src/components/NoteForm.jsx
+++ fullstackopen-workshops/part2/notes-app/src/components/NoteForm.jsx
@@ -1,12 +1,31 @@
-const NoteForm = ({ onSubmit, handleChange, value}) => {
+import { useState } from "react";
+
+//onSubmit is a function that will be called when the form is submitted
+//it will be passed the new note object
+
+const NoteForm = ({ onSubmit }) => {
+  const [newNote, setNewNote] = useState("");
+
+  const mySubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ 
+      content: newNote,
+      important:  Math.random() < 0.5,
+    });
+
+    setNewNote("");
+  }
   return (
     <div>
       <h2>Create a new note</h2>
 
-      <form onSubmit={onSubmit}>
+      <form onSubmit={mySubmit}>
+        <input />
         <input
-          value={value}
-          onChange={handleChange}
+          className="someThing"
+          placeholder="enter something here"
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
         />
         <button type="submit">save</button>
       </form>
