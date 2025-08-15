5fa2f740bda6246cac58d1ffd014fafcb6c6b4e4
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-15 17:11:30 +0545

    Add: jest globals true && babelrc & add deepfreeze library

--- fullstackopen-workshops/part-6/redux-notes/eslint.config.js
+++ fullstackopen-workshops/part-6/redux-notes/eslint.config.js
@@ -15,7 +15,11 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      // globals: globals.browser,
+      globals: { 
+        ...globals.browser, //browser globals
+        ...globals.jest,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
