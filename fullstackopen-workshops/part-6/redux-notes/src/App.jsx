c40864a4acd28f486271f4b0f0f2da45d7feb107
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-19 16:54:40 +0545

    Add:  filter state & three radio button

--- fullstackopen-workshops/part-6/redux-notes/src/App.jsx
+++ fullstackopen-workshops/part-6/redux-notes/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import { useSelector, useDispatch } from 'react-redux';
 // import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
 import NoteForm from './components/NoteForm';
@@ -6,14 +6,45 @@ import Notes from './components/Notes';
 
 
 const App = () => {
- 
- 
- 
+
+  const [filter, setFilter] = useState("ALL");
+
+  const filterSelected =(filter) => { 
+    console.log("clicked radio is", filter);
+    setFilter(filter);
+  }
+
 
   return (
     <div>
+      {/* copy paste from fullstackopen */}
+      {/* div vitra teen ota radio button rakhe ko xu */}
+      {/* what's the difference between radio button and check box  */}
+      <div>
+        all
+        <input 
+          type='radio'
+          name='filter'
+          onChange={() => filterSelected("ALL")}
+        />
+        important
+        <input 
+          type='radio'
+          // it all depends on value of filter 
+          name='filter'
+          onChange={() => filterSelected("IMPORTANT")}
+        />
+        nonimportant
+        <input
+          type='radio'
+          name='filter'
+          onChange={() => filterSelected("NONIMPORTANT")} 
+        
+        />
+
+      </div>
       <NoteForm />
-      <Notes/>
+      <Notes filter={filter}/>
     </div>
   );
 };
