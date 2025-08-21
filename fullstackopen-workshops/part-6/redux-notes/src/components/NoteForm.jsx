f24858db9b5100bc7c82313c5330ceb7dc605b2d
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-21 17:21:24 +0545

    Add: import noteServices & call func createNew & dispatch the newly created Note

--- fullstackopen-workshops/part-6/redux-notes/src/components/NoteForm.jsx
+++ fullstackopen-workshops/part-6/redux-notes/src/components/NoteForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector , useDispatch } from "react-redux";
 import { createNote} from "../reducers/noteReducer.js";
+import noteServices from '../services/notes';
 const NoteForm = () => {
 
   //state rw useState and this is the advantage of using redux
@@ -17,7 +18,10 @@ const NoteForm = () => {
       // id: store.getState().length + 1,
       id: notes.length + 1,
     };
-    dispatch(createNote(newNote));
+    //server mw ni raknu paryo ni jatha and baneko aauxa and then we will resolve the promise and show the result 
+    noteServices.createNew(newNote).then((myNote) => { 
+      dispatch(createNote(myNote));
+    })
     e.target.myInput.value = ""; // clear the input field after adding a note
   };
   return (
