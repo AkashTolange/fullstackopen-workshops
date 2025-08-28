83af6ee337eb8d5c88978c2253a249efa873aa34
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-28 15:58:40 +0545

    Adding: new note via React Query and updating toggleimportance

--- fullstackopen-workshops/part-6/react-query/src/ReactQueryApp.jsx
+++ fullstackopen-workshops/part-6/react-query/src/ReactQueryApp.jsx
@@ -2,38 +2,67 @@
 //useEffect ko satta useQuery chalaune hae 
 
 
-import { useQuery } from "@tanstack/react-query"
+import {  useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 // import axios from 'axios'
 
-import {getNotes} from './request';
+import {getNotes, createNote, updateNote} from './request';
 
 
 const App =() =>  { 
-  const addNote = async(event) => { 
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value ="";
-    console.log(content);
-  };
-  
-  const toggleImportance = (note) => { 
-    console.log("toggle importance of ", note.id);
-  };
-  
+
+  const queryClient = useQueryClient(); 
+
   // const notes =[]; instead of empty array and useQuery is like hook that takes object and 
   //query fnc lae j func pass garyyou, tyo func le return gareko data, notes key mw haldinw ne rahexa
   const result = useQuery({ 
     queryKey: ["notes"],
     queryFn: getNotes,
   });
+  // const newNoteMutation = useMutation({ mutationFn: createNote })
+  //yo post ko lage
+  const newNoteMutation = useMutation({ 
+    mutationFn: createNote,
+    onSuccess: () => { 
+      queryClient.invalidateQueries({ queryKey: ["notes"]});
+    },
+  })
+
+  //version v4 rw v5 mw differenct huncha hae syntax different huncha
+
+  //yo update ko lage and for toggle importance ko lage
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+      onSuccess: () => { 
+      queryClient.invalidateQueries({ queryKey: ["notes"]})
+    },
+  })
+
+
+  const addNote = async(event) => { 
+    event.preventDefault();
+    const content = event.target.note.value;
+    //for post we need to do 
+    newNoteMutation.mutate({ content, important: true}) //this obj will be passed to createNote below 
+    event.target.note.value ="";
+    console.log(content);
+  };
+  
+  const toggleImportance = (note) => { 
+    console.log("toggle importance of ", note.id);
+    const updatedNote = { ...note, important: !note.important };
+    updateNoteMutation.mutate(updatedNote);
+  };
+  
+  
 
   console.log(JSON.parse(JSON.stringify(result)));
 
   if(result.isLoading) { 
     return <div>loading data...</div>;
   }
+
   
-  const notes = result.data;
+  const notes = result.data || [];
 
   return ( 
     <div>
@@ -42,11 +71,13 @@ const App =() =>  {
         <input name="note"/>
         <button type="submit">add</button>
       </form>
-      {notes.map(note => 
+      {notes.map(note => {
+        return (
         <li key={note.id} onClick={() => toggleImportance(note)}>
           {note.content}
           <strong>{note.important ? 'important' : ''}</strong>
         </li>
+        )}
       )}
     </div>
   )
--- fullstackopen-workshops/part-6/react-query/src/request.js
+++ fullstackopen-workshops/part-6/react-query/src/request.js
@@ -1,7 +1,20 @@
-import axios from "react";
+import axios from 'axios'
+const baseUrl = "http://localhost:3001/notes";
 
+//to get 
 export const getNotes = () =>
   //query fnc lae j func pass garyyou, tyo func le return gareko data, notes key mw haldinw ne rahexa
-  axios.get("http://localhost:3001/notes").then((res) => {
+  axios.get(baseUrl).then((res) => {
     return res.data;
   });
+
+  //to post 
+  export const createNote =(newNote) => { 
+  return axios.post(baseUrl, newNote).then((res) =>  res.data)
+  }
+
+export const updateNote =(updatedNote) => { 
+   return axios
+    .put(`${baseUrl}/${updatedNote.id}`, updatedNote)
+    .then((res) => res.data);
+}
