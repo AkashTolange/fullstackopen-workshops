b9a97729f90715446082613132a05ae44ff98a53
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-03 16:34:40 +0545

    Add: useNavigate to programmatically navigate to a url

--- fullstackopen-workshops/part-7/react-router/src/App.jsx
+++ fullstackopen-workshops/part-7/react-router/src/App.jsx
@@ -1,33 +1,34 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Notes from "./Notes";
 import Note from "./Note";
+import { useState } from "react";
+import Login from "./Login";
 
-const notes =[
+const notes = [
   {
-    content: 'the app state is in redux store',
+    content: "the app state is in redux store",
     important: true,
     id: 1,
   },
-  { 
-    content: 'state changes are made with actions',
+  {
+    content: "state changes are made with actions",
     important: false,
     id: 2,
   },
   {
-    content: 'one more',
+    content: "one more",
     important: true,
     id: 3,
-  }
-]
-
+  },
+];
 
 //home
 const Home = () => {
-  return ( 
-  <div>
-    <h2>TKTL notes app</h2>
-  </div>
-  )
+  return (
+    <div>
+      <h2>TKTL notes app</h2>
+    </div>
+  );
 };
 
 //notes
@@ -42,13 +43,15 @@ const Home = () => {
 //users
 const Users = () => {
   return (
-  <div>
-    <h2>Users</h2>
-  </div>
+    <div>
+      <h2>Users</h2>
+    </div>
   );
 };
 //
 const App = () => {
+  const [user, setUser] = useState(null);
+
   const padding = {
     padding: 5,
   };
@@ -65,14 +68,26 @@ const App = () => {
         <Link style={padding} to="/users">
           users
         </Link>
+        {user ? (
+          <em>{user} logged in </em>
+        ) : (
+          <Link style={padding} to="/login">
+            Login
+          </Link>
+        )}
       </div>
 
       <Routes>
-        <Route path="/notes" element={<Notes notes={notes}/>} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
         {/* need to build another Note componenet haha */}
-        <Route path="/notes/:id" element={<Note notes={notes}/>}/>
+        <Route path="/notes/:id" element={<Note notes={notes} />} />
 
-        <Route path="/users" element={<Users />} />
+        {/* <Route path="/users" element={<Users />} /> */}
+        {/* <Route path="/login" element={<Login setUser={setUser}/>} /> */}
+        <Route 
+          path="/users"          //example navigating to a url
+          element={user ? <Users/> : <Navigate replace to="/login" />}
+        />
         <Route path="/" element={<Home />} />
       </Routes>
 
---
+++ fullstackopen-workshops/part-7/react-router/src/Login.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+const Login = ({setUser}) => { 
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) =>{ 
+        event.preventDefault();
+        console.log(event.target.username.value)
+        setUser(event.target.username.value)
+        navigate('/')
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="username"/>
+            <button>submit</button>
+        </form>
+    )
+}
+
+export default Login;
