74e5aee164ef96e6646d0f6c148afe0eed91fe15
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-04 15:45:59 +0545

    Add: create custom hook for counter application

--- fullstackopen-workshops/part-7/custom-hooks/src/App.jsx
+++ fullstackopen-workshops/part-7/custom-hooks/src/App.jsx
@@ -1,28 +1,29 @@
-import {useState} from 'react';
-
+// import {useState} from 'react';
+//our customhooks
+import useCounter from './hooks/useCounter';
 
 const App = () => {
 
   //DRY : don't repeat yourself
 
-  const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
+  const counter1 = useCounter(0);
+  const counter2 = useCounter(0);
   return ( 
     <>
     <h5>first counter</h5>
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(counter - 1)}>minus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      <div>{counter1.value}</div>
+      <button onClick={counter1.increase}>plus</button>
+      <button onClick={counter1.decrease}>minus</button>
+      <button onClick={counter1.zero}>zero</button>
     </div>
     <h5>second counter</h5>
     <div>
-      <div>{counter2}</div>
-      <button onClick={() => setCounter2(counter2 + 1)}>plus</button>
-      <button onClick={() => setCounter2(counter2 - 1)}>minus</button>
-      <button onClick={() => setCounter2(0)}>zero</button>
-    </div>
+      <div>{counter2.value}</div>
+      <button onClick={counter2.increase}>plus</button>
+      <button onClick={counter2.decrease}>minus</button>
+      <button onClick={counter2.zero}>zero</button>
+    </div> 
     
     </>
   )
