b6b1d8d6741dca656938bd1c8a7e4242c129c612
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-18 16:43:28 +0545

    Add: Note model & centralize all models related code

---
+++ fullstackopen-workshops/part-13/notes-server/models/note.js
@@ -0,0 +1,41 @@
+
+const { Model, DataTypes} = require("sequelize");
+
+const {sequelize} = require("../utils/db")
+// Middleware
+// app.use(express.json());
+
+//schema: note Schema
+class Note extends Model {}
+Note.init(
+  {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: { 
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: { 
+      type: DataTypes.BOOLEAN,
+    },
+    date: { 
+      type: DataTypes.DATE,
+    },
+  },
+  { 
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+
+
+//what does it do?? database mw connect gare rw  Note model ko , notes vanne table xa ki xaina hernw janxa
+//mathe ko jasto schema banae dinxa
+Note.sync();
+
+module.exports = Note;
