35f4e052de57c7f7c73b69340ec4bc3b7718b32f
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-18 16:45:07 +0545

    Add: refactoring the code :Util controllers & models directory

--- fullstackopen-workshops/part-13/notes-server/index.js
+++ fullstackopen-workshops/part-13/notes-server/index.js
@@ -1,121 +1,30 @@
 // index.js
-require("dotenv").config();
-// const { Pool } = require("pg");
-
-
-//mongoose lae lera ko jastae ho 
-const { Sequelize, QueryTypes, Model, DataTypes} = require("sequelize");
-
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
 
+const notesRouter = require("./controllers/notes")
+const {PORT} = require("./utils/config")
+const {connectToDatabase} = require('./utils/db')
 // PostgreSQL connection pool
 // const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL,
 // });
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { 
-  dialectOptions: { 
-    ssl: { 
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+
+app.use("/api/notes", notesRouter);
 
 
-// Middleware
-app.use(express.json());
+const start = async() => { 
+  await connectToDatabase()
+  app.listen(PORT, () => { 
+    console.log( `server running on port ${PORT}`)
+  })
+}
 
-//schema: note Schema
-class Note extends Model {}
-Note.init(
-  {
-    id: { 
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: { 
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: { 
-      type: DataTypes.BOOLEAN,
-    },
-    date: { 
-      type: DataTypes.DATE,
-    },
-  },
-  { 
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "note",
-  }
-);
+start();
 
-
-//what does it do?? database mw connect gare rw  Note model ko , notes vanne table xa ki xaina hernw janxa
-//mathe ko jasto schema banae dinxa
-Note.sync();
-
-
-
-// GET all notes
-app.get("/api/notes", async (req, res) => {
-  try {
-    // const result = await sequelize.query("SELECT * FROM notes",{
-    //   type: QueryTypes.SELECT
-    // });
-    // res.json(result);
-
-    //schema banayo ane chalayo matra
-    const notes = await Note.findAll();
-    res.json(notes);
-  } catch (err) {
-    console.error("Error fetching notes:", err.message);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.post("/api/notes", async(req, res) => { 
-  console.log(req.body);
-  const note = await Note.create(req.body);
-  res.json(note);
-})
-
-
-//to get id
-app.get("/api/notes/:id", async(req, res) => { 
-  const note = await Note.findByPk(req.params.id);
-  if ( note ) {
-    res.json(note);
-  } else { 
-    res.status(404).send("no data found");
-  }
-});
-
-//put 
-app.put('/api/notes/:id', async(req, res) => { 
-  const note = await Note.findByPk(req.params.id);
-  // console.log(note);
-  // console.log(note.toJSON());
-  // console.log(JSON.stringify(note))
-  console.log(JSON.stringify(note, null, 2));
-  
-  if ( note ) { 
-    note.important = req.body.important
-    await note.save()
-    res.json(note)
-  } else { 
-    res.status(404).end()
-  }
-})
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// // Start server
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
