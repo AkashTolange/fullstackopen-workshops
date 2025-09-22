004e6d1b1eb6b98848a3fb010d508e4287644eff
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-22 12:03:43 +0545

    Added: relation between User and Note

--- fullstackopen-workshops/part-13/notes-server/models/index.js
+++ fullstackopen-workshops/part-13/notes-server/models/index.js
@@ -2,6 +2,7 @@ const  Note  = require("./note")
 const  User  = require("./user")
 
 //to to show relation between note and user
+//added relation between User and Note
 
 User.hasMany(Note)                                 //Note table ko record chae belongs to user vane ko 
 Note.belongsTo(User)                               //rw maile yade kunae new record thape vane it belongs to User too
